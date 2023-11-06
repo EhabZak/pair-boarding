3851ded1e57101814280c08bbb509d05182ae13d
ehab <ehab41976@gmail.com>
2023-11-05 21:59:09 -0800

    3rd

--- Pairboarding-practice.js
+++ Pairboarding-practice.js
@@ -65,3 +65,11 @@ function digitalRoot(num) {
 
     return encoded;
   }
+
+  // fibsSum
+
+  /*
+Write a function, fibsSum(n), that finds the sum of the first n fibonacci numbers
+ recursively. Assume n > 0. Note that for this problem, the fibonacci sequence starts
+ with [1, 1].
+  */
