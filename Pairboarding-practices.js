7194755abc44a14b771a92a4d70e11c9d1edd9ee
ehab <ehab41976@gmail.com>
2023-11-11 19:15:26 -0800

    commit 2

--- Pairboarding-practices.js
+++ Pairboarding-practices.js
@@ -96,3 +96,9 @@ function fibsSum(n) {
 
   return fibsSum(n - 1) + fibsSum(n - 2) + 1;
 }
+
+//! **`fibsSum`**
+
+/*Write a function, `fibsSum(n)`, that finds the sum of the first n
+fibonacci numbers recursively. Assume n > 0. Note that for this problem,
+the fibonacci sequence starts with `[1, 1]`.*/
