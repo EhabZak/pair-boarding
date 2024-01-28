c47b267930cd34920054992861706e19f4074a81
ehab <ehab41976@gmail.com>
2024-01-28 00:12:06 -0800

    problems

--- HackerRank.py
+++ HackerRank.py
@@ -4,7 +4,7 @@ import random
 import re
 import sys
 
-#! 1-
+#! 1- Plus Minus
 
 '''
 Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
@@ -99,33 +99,121 @@ negative:2/6=0.333333  and zeros: 0.166667
 # # arr = [1,1,0,-1,-1]
 # arr = [-4, 3, -9, 0, 4, 1]
 
-print(plusMinus(arr))
+# print(plusMinus(arr))
 
-#! Chat Gpt solutions
+# #! Chat Gpt solutions
 
-def plusMinus(arr):
+# def plusMinus(arr):
 
-    #! solution 1
-    n = len(arr)
-    # positive_count = sum(1 for num in arr if num > 0) # 1 is a place holder 
-    # negative_count = sum(1 for num in arr if num < 0)
-    # zero_count = n - positive_count - negative_count
-    #! solution 2
+#     #! solution 1
+#     n = len(arr)
+#     # positive_count = sum(1 for num in arr if num > 0) # 1 is a place holder
+#     # negative_count = sum(1 for num in arr if num < 0)
+#     # zero_count = n - positive_count - negative_count
+#     #! solution 2
 
-    positive_count = sum( num > 0 for num in arr)
-    negative_count = sum (num < 0 for num in arr)
-    zero_count = n - positive_count - negative_count
+#     positive_count = sum( num > 0 for num in arr)
+#     negative_count = sum (num < 0 for num in arr)
+#     zero_count = n - positive_count - negative_count
 
 
-    positive_ratio = positive_count / n
-    negative_ratio = negative_count / n
-    zero_ratio = zero_count / n
+#     positive_ratio = positive_count / n
+#     negative_ratio = negative_count / n
+#     zero_ratio = zero_count / n
 
-    # Print the ratios with 6 decimal places
-    print("{:.6f}".format(positive_ratio))
-    print("{:.6f}".format(negative_ratio))
-    print("{:.6f}".format(zero_ratio))
+#     # Print the ratios with 6 decimal places
+#     print("{:.6f}".format(positive_ratio))
+#     print("{:.6f}".format(negative_ratio))
+#     print("{:.6f}".format(zero_ratio))
 
-# Example usage:
-arr = [-4, 3, -9, 0, 4, 1]
-plusMinus(arr)
+# # Example usage:
+# arr = [-4, 3, -9, 0, 4, 1]
+# plusMinus(arr)
+
+
+
+#! 2-Mini-Max Sum
+
+'''
+1- iterate over the list add the numbers except for one number
+iterate and remove one number every time you can have an i and increment it to remove the next number
+we need an array to collect the numbers and then we return the max and min in that array using min and max
+we can deduct the value of item at i after adding everything in the arr
+'''
+
+
+#!
+
+'''
+Given five positive integers, find the minimum and maximum values that can be calculated by
+summing exactly four of the five integers. Then print the respective minimum and maximum
+values as a single line of two space-separated long integers.
+
+Example
+
+The minimum sum is  and the maximum sum is . The function prints
+
+16 24
+Function Description
+
+Complete the miniMaxSum function in the editor below.
+
+miniMaxSum has the following parameter(s):
+
+arr: an array of  integers
+Print
+
+Print two space-separated integers on one line: the minimum sum and the maximum sum of  of  elements.
+
+Input Format
+
+A single line of five space-separated integers.
+
+Constraints
+
+Output Format
+
+Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
+
+Sample Input
+
+1 2 3 4 5
+Sample Output
+
+10 14
+
+'''
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+arr = [1,3,5,7,9]
+def miniMaxSum(arr):
+    # nums = []
+
+    # total = sum(arr)
+    # # print (total)
+    # for n in arr:
+    #     number = total - n
+    #     nums.append(number)
+
+    # max1 = max(nums)
+    # min1 = min(nums)
+    # print (min1, max1)
+    # Write your code here
+
+    #! another solution (GPT)
+
+    total_sum = sum(arr)
+    min_sum = total_sum - max(arr)
+    max_sum = total_sum - min(arr)
+
+    print(min_sum, max_sum)
+
+# if __name__ == '__main__':
+
+#     arr = list(map(int, input().rstrip().split()))
+
+miniMaxSum(arr)
--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -5,26 +5,25 @@ from collections import defaultdict, Counter
 class Solution:
     def frequencySort(self, s: str) -> str:
         n = len(s)
-        c = Counter(s)
+        count = Counter(s)
+
+
         bucket = [[] for _ in range(n+1)]
-        print(bucket)
 
-
-        for char,freq in c.items():
-            print(char)
+        for char, freq in count.items():
             bucket[freq].append(char)
-            print(bucket)
 
         res = ''
-        for freq in range(n,0,-1):
-            print('======',freq)
-            for char in bucket[freq]:
-                res += char*freq
+
+        for index in range (n,0,-1):
+            for char in bucket[index]:
+                # print(char)
+                res += char*index
+
         return res
 
-
-s ="Abcddeeeee"
+# s ="Abcddeeeee"
 # s = "Aabb"
-# s = "tree"
+s = "tree"
 output = Solution().frequencySort(s)
 print(output)
