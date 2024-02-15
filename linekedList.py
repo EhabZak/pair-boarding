2f6fa6337fcfca970a703b4c1477b1ca6674e2f4
ehab <ehab41976@gmail.com>
2024-02-15 10:40:08 -0800

    problem

--- linekedList.py
+++ linekedList.py
@@ -377,68 +377,68 @@ Both list1 and list2 are sorted in non-decreasing order.
 '''
 
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 == None: return list2
-        if list2 == None: return list1
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+#     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+#         if list1 == None: return list2
+#         if list2 == None: return list1
 
-        if list1.val < list2.val:
-            next1 = list1.next # this is a pointer
-            list1.next = self.mergeTwoLists(next1,list2)
-            return list1
-        else:
-            next2 = list2.next # this is a pointer
-            list2.next = self.mergeTwoLists(list1,next2)
-            return list2
+#         if list1.val < list2.val:
+#             next1 = list1.next # this is a pointer
+#             list1.next = self.mergeTwoLists(next1,list2)
+#             return list1
+#         else:
+#             next2 = list2.next # this is a pointer
+#             list2.next = self.mergeTwoLists(list1,next2)
+#             return list2
 
-        #! iterative solution
-        # Initialize a dummy node to help with the iteration
-        # dummy = ListNode()
-        # # Initialize a pointer to the dummy node
-        # current = dummy #! this is a pointer
-
-
-        # # Iterate until both lists are exhausted
-        # while list1 and list2:
-        #     if list1.val < list2.val:
-        #         current.next = list1
-        #         list1 = list1.next
-        #     else:
-        #         current.next = list2
-        #         list2 = list2.next
-        #     current = current.next
-
-        # # Attach the remaining nodes from either list1 or list2
-        # if list1:
-        #     current.next = list1
-        # elif list2:
-        #     current.next = list2
-
-        # # Return the merged list starting from the next of the dummy node
-        # return dummy.next
+#         #! iterative solution
+#         # Initialize a dummy node to help with the iteration
+#         # dummy = ListNode()
+#         # # Initialize a pointer to the dummy node
+#         # current = dummy #! this is a pointer
 
 
+#         # # Iterate until both lists are exhausted
+#         # while list1 and list2:
+#         #     if list1.val < list2.val:
+#         #         current.next = list1
+#         #         list1 = list1.next
+#         #     else:
+#         #         current.next = list2
+#         #         list2 = list2.next
+#         #     current = current.next
 
-# list1 = [1,2,4] # [1,1,2,3,4,4]
-# list2 = [1,3,4]
-# Create ListNode objects for list1 and list2
-list1 = ListNode(1)
-list1.next = ListNode(2)
-list1.next.next = ListNode(4)
+#         # # Attach the remaining nodes from either list1 or list2
+#         # if list1:
+#         #     current.next = list1
+#         # elif list2:
+#         #     current.next = list2
 
-list2 = ListNode(1)
-list2.next = ListNode(3)
-list2.next.next = ListNode(4)
+#         # # Return the merged list starting from the next of the dummy node
+#         # return dummy.next
 
-# Instantiate Solution class
-sol = Solution()
 
-# Call mergeTwoLists method
-merged_list = sol.mergeTwoLists(list1, list2)
-while merged_list is not None:
-    print(merged_list.val, end=" ")
-    merged_list = merged_list.next
+
+# # list1 = [1,2,4] # [1,1,2,3,4,4]
+# # list2 = [1,3,4]
+# # Create ListNode objects for list1 and list2
+# list1 = ListNode(1)
+# list1.next = ListNode(2)
+# list1.next.next = ListNode(4)
+
+# list2 = ListNode(1)
+# list2.next = ListNode(3)
+# list2.next.next = ListNode(4)
+
+# # Instantiate Solution class
+# sol = Solution()
+
+# # Call mergeTwoLists method
+# merged_list = sol.mergeTwoLists(list1, list2)
+# while merged_list is not None:
+#     print(merged_list.val, end=" ")
+#     merged_list = merged_list.next
