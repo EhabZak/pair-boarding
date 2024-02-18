4151e3238fefd1f942dbcdcf71cb5d457ecc2e19
ehab <ehab41976@gmail.com>
2024-02-17 19:13:53 -0800

    problem

--- DSandA.py
+++ DSandA.py
@@ -1849,6 +1849,39 @@ sol = Solution()
 result = sol.postorderTraversal(root)
 print(result)  # Output should be [1, 3, 2]
 
+
+#!
+
+'''
+102. Binary Tree Level Order Traversal
+Medium
+Topics
+Companies
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-1000 <= Node.val <= 1000
+'''
+
 #! 10-1376. Time Needed to Inform All Employees -(Lecture) - (Medium)
 #! 11-752. Open the Lock -(Lecture) - (Medium)
 #! 12-554. Brick Wall -(Lecture) - (Medium)
