8616bbf4086092fd21ea72f4b3696b91242ba378
ehab <ehab41976@gmail.com>
2024-02-22 22:09:44 -0800

    problem

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -3,3 +3,18 @@ from typing import Optional, List
 from collections import defaultdict, Counter
 
 
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        queue = deque()
+        queue.append(root)
+        output = []
+        while queue:
+            # shift out of the queue
+            node = queue.popleft()
+            if node:
+                output.append(node.val)
+                #adding children to the queue
+                queue.append(node.left)
+                queue.append(node.right)
+        return output
+
