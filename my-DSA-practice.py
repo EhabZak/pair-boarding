d36bd9e7275416c16ab78fa0b03b29d207886680
ehab <ehab41976@gmail.com>
2024-02-27 13:16:05 -0800

    problem

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -31,31 +31,6 @@ Constraints:
 The number of nodes in the tree is in the range [0, 2000].
 -1000 <= Node.val <= 1000
 '''
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        queue = deque()
-        queue.append(root)
-        output = []
-
-        while queue:
-            queueLen = len(queue)
-            level = []
-
-            for i in range(queueLen):
-                node = queue.popleft()
-
-                if node:
-                    level.append(node.val)
-                    queue.append(node.left)
-                    queue.append(node.right)
-            if level:
-                output.append(level)
-        return output
 
 
 
@@ -66,10 +41,4 @@ class Solution:
 
 
 
-root = TreeNode(3)
-root.left = TreeNode(9)
-root.right = TreeNode(20)
-root.right.left = TreeNode(15)
-root.right.right = TreeNode(7)
-solution = Solution()
-print(solution.levelOrder(root))
+
