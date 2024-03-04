fa63c1fe090e7a2c467648664a12cdbbeea535f0
ehab <ehab41976@gmail.com>
2024-03-04 12:59:55 -0800

    problems

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -7,22 +7,7 @@ import math
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        pts= []
-
-        for x, y in points:
-            dist = math.sqrt(abs(x-0)**2 + abs(y-0)**2)
-            pts.append([dist,x , y] )
-
-        res = []
-        heapq.heapify(pts)
-        # print(pts)
-
-        for i in range(k):
-            dist, x , y = heapq.heappop(pts)
-            res.append([x,y])
-
-        return res
-
+        
 
 
 
