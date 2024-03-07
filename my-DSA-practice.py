f9aca6413ae7030cc4831bde71200e00f9d1961e
ehab <ehab41976@gmail.com>
2024-03-07 13:36:37 -0800

    problems

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -5,26 +5,4 @@ import heapq
 import math
 
 
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        subset = []
 
-        def backtrack(i):
-            #base case
-            if i >= len(nums):
-                ans.append(subset.copy())
-                return
-            # to include nums[i]
-            subset.append(nums[i])
-            backtrack(i+1)
-            subset.pop()
-            # exclude
-            backtrack(i+1)
-        backtrack(0)
-        return ans
-
-
-nums = [1,2,3]
-sol = Solution()
-print(sol.subsets(nums))
