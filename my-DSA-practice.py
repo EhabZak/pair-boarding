795358b160477e1f310b22fdbf27965e8c09d6fd
ehab <ehab41976@gmail.com>
2024-03-23 05:22:41 -0700

    problems

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -5,3 +5,9 @@ import heapq
 import math, random
 
 
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.dll =DoubleLinkedList() # creates only one double linked list # there is only one instance of double linked list
+        self.hash = {}
