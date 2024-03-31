5eccdbccaf00fa15a37de39dd0c63b4b4283a0fa
ehab <ehab41976@gmail.com>
2024-03-31 02:23:02 -0700

    problems

--- DSandA.py
+++ DSandA.py
@@ -2906,7 +2906,7 @@ At most 2 * 105 calls will be made to get and put.
 # print(cache.get(3))     # Get value associated with key=3 (Expected output: 3)
 # print(cache.get(4))     # Get value associated with key=4 (Expected output: 4)
 
-#! 36- 200. Number of Islands (Medium) (algo: matrix dfs)  (time complexity O(1))
+#! 36- 200. Number of Islands (Medium) (algo: matrix dfs)  (time complexity O(ROWS * COLS))
 
 '''
 200. Number of Islands
@@ -3035,6 +3035,89 @@ grid[i][j] is '0' or '1'.
 # print(sol.numIslands(grid))
 
 
+#! 37- 695. Max Area of Island (Medium) (algo: matrix dfs)  (time complexity O(ROWS * COLS))
+
+'''
+695. Max Area of Island
+Medium
+Topics
+Companies Amazon bloomberg facebook
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+The area of an island is the number of cells with a value 1 in the island.
+
+Return the maximum area of an island in grid. If there is no island, return 0.
+
+
+
+Example 1:
+
+
+Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Output: 6
+Explanation: The answer is not 11, because the island must be connected 4-directionally.
+Example 2:
+
+Input: grid = [[0,0,0,0,0,0,0,0]]
+Output: 0
+
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 50
+grid[i][j] is either 0 or 1.
+
+'''
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+
+        def dfs(r,c):
+            if inbound(r,c) or grid[r][c] ==0:
+                return 0
+            pos = (r,c)
+            if pos in visited:
+                return 0
+            size = 1
+            visited.add(pos)
+
+            directions = ((1,0),(0,1),(-1,0),(0,-1))
+            for dir in directions:
+                newRow = r + dir[0]
+                newCol = c + dir[1]
+                size += dfs(newRow, newCol)
+            return size
+
+
+        def inbound(r,c):
+            rowInbound = 0 < r < rows
+            colInbound = 0 < c < cols
+            return rowInbound and colInbound
+
+        largestIsland = 0
+        visited = set()
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                  largestIsland = max(largestIsland, dfs(r,c))
+
+        return largestIsland
+
+
+grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+        [0,0,0,0,0,0,0,1,1,1,0,0,0],
+        [0,1,1,0,1,0,0,0,0,0,0,0,0],
+        [0,1,0,0,1,1,0,0,1,0,1,0,0],
+        [0,1,0,0,1,1,0,0,1,1,1,0,0],
+        [0,0,0,0,0,0,0,0,0,0,1,0,0],
+        [0,0,0,0,0,0,0,1,1,1,0,0,0],
+        [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+
+sol = Solution()
+print(sol.maxAreaOfIsland(grid))
 
 
 #! 11-752. Open the Lock -(Lecture) - (Medium)
