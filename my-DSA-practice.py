aef0013583a11bcf9b1f3d050b9f2ddb4dcb4936
ehab <ehab41976@gmail.com>
2024-03-31 01:36:36 -0700

    problems

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -12,40 +12,27 @@ class Solution:
         def dfs(r,c):
             if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]):
                 return
-            if grid[r][c] =="0":
+            if grid[r][c] == '0':
                 return
-
-            grid[r][c]= '0'
+            grid[r][c] = '0'
 
             dfs(r-1,c)
-            dfs(r+1, c)
-            dfs(r, c-1)
+            dfs(r+1,c)
+            dfs(r,c-1)
             dfs(r,c+1)
-
-
-
 
         for r in range(len(grid)):
             for c in range(len(grid[0])):
-                if grid[r][c] == "1":
-                    count +=1
+                if grid[r][c] =='1':
+                    count+=1
                     dfs(r,c)
         return count
 
 
-            # direction check
 
 
-            # coordinates and do the thing
-        visited = set()
-        count = 0
 
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
 
-                if dfs(row, col):
-                    count +=1
-        return count
 
 
 
