c51006c431f742406e8a8faf31c1e3eff59a32fa
ehab <ehab41976@gmail.com>
2024-04-05 05:07:50 -0700

    problems

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -5,43 +5,4 @@ import heapq
 import math, random
 
 
-class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
 
-        rows, cols = len(grid), len(grid[0])
-        largestIsland = 0
-
-        def dfs(r,c):
-            if r < 0 or r >= rows or c < 0 or c >= cols:
-                return 0
-
-            if grid[r][c] == 0:
-                return 0
-
-            grid[r][c] = 0
-
-            size = 1 + dfs(r+1,c) + dfs (r-1,c)+ dfs(r, c+1)+ dfs(r,c-1)
-
-
-
-            return size
-
-        for r in range(rows):
-            for c in range(cols):
-                largestIsland= max(largestIsland, dfs(r,c))
-        print (grid)
-        return largestIsland
-
-
-
-grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],
-        [0,0,0,0,0,0,0,1,1,1,0,0,0],
-        [0,1,1,0,1,0,0,0,0,0,0,0,0],
-        [0,1,0,0,1,1,0,0,1,0,1,0,0],
-        [0,1,0,0,1,1,0,0,1,1,1,0,0],
-        [0,0,0,0,0,0,0,0,0,0,1,0,0],
-        [0,0,0,0,0,0,0,1,1,1,0,0,0],
-        [0,0,0,0,0,0,0,1,1,0,0,0,0]]
-
-sol = Solution()
-print(sol.maxAreaOfIsland(grid))
