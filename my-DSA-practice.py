2a9e944cf6b95c81c5c9f92c578e0a1a5a912cce
ehab <ehab41976@gmail.com>
2024-04-05 05:07:15 -0700

    problems

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -9,63 +9,28 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
 
         rows, cols = len(grid), len(grid[0])
-
-        # dfs helper function an three base cases
-        def dfs(r,c):
-            if not inbound(r,c) or grid[r][c] == 0:
-                return 0
-            pos = (r,c)
-            if pos in visited:
-                return 0
-
-            visited.add(pos)
-            size = 1
-
-            directions = ((0,1),(0,-1),(1,0),(-1,0))
-            for dir in directions:
-                realRow = r + dir[0]
-                realCol = c + dir[1]
-                size += dfs(realRow, realCol)
-            return size
-
-
-
-
-        def inbound(r,c):
-            rowIn = 0 <= r < rows
-            colIn = 0 <= c < cols
-            return rowIn and colIn
-
-
-
-
-        # out of bound, in viisted , if value is == 0
-        #in bound helper function
-        # directions check
-        # position check in visited
-
-        # coordinates and do the thing
-
         largestIsland = 0
-        visited = set()
+
+        def dfs(r,c):
+            if r < 0 or r >= rows or c < 0 or c >= cols:
+                return 0
+
+            if grid[r][c] == 0:
+                return 0
+
+            grid[r][c] = 0
+
+            size = 1 + dfs(r+1,c) + dfs (r-1,c)+ dfs(r, c+1)+ dfs(r,c-1)
+
+
+
+            return size
 
         for r in range(rows):
             for c in range(cols):
-                largestIsland = max(largestIsland, dfs(r,c))
-
+                largestIsland= max(largestIsland, dfs(r,c))
+        print (grid)
         return largestIsland
-
-
-
-
-        # return the largest island
-
-
-
-
-
-
-
 
 
 
