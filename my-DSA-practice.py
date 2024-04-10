241afdb516f59ba16c0e24a68d9ec3de6766976e
ehab <ehab41976@gmail.com>
2024-04-10 01:18:29 -0700

    problems

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -7,49 +7,57 @@ import math, random
 
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        rows, cols = len(heights), len(heights[0])
+        rows , cols = len(heights),  len(heights[0])
 
-        def dfs(r,c,visited,prevHeight):
-            if not inbound(r,c) or (r,c) in visited or heights[r][c]< prevHeight:
+        # dfs and base cases
+        def dfs (r,c, visited, prevHight):
+            if not inbound(r,c) or (r,c) in visited or heights[r][c]< prevHight:
                 return
-
             visited.add((r,c))
 
-            directions = ((0,1), (0,-1),(1,0),(-1,0))
+            # directions
+            directions = ((0,1),(0,-1),(1,0),(-1,0))
             for dir in directions:
-                newRow = r +dir[0]
+                newRow = r + dir[0]
                 newCol = c + dir[1]
-                dfs(newRow, newCol, visited,heights[r][c])
+                dfs(newRow,newCol,visited,heights[r][c])
 
 
+
+        # inbound
         def inbound(r,c):
             rowIn = 0 <= r < rows
             colIn = 0 <= c < cols
             return rowIn and colIn
 
 
+
+
+        #sets
         res = []
         pacific = set()
         atlantic = set()
 
+        # starting points
         for c in range(cols):
-            # north side
-            dfs(0,c,pacific,heights[0][c])
+            # north
+            dfs(0,c,pacific, heights[0][c])
 
-            # south side
-            dfs(rows-1,c,atlantic,heights[rows-1][c])
+            #south
+            dfs(rows-1,c,atlantic, heights[rows-1][c])
+
 
         for r in range(rows):
+            # west
             dfs(r,0,pacific,heights[r][0])
-
+            #east
             dfs(r,cols-1,atlantic,heights[r][cols-1])
-
-        for r in range(rows):
-            for c in range(cols):
+            pass
+        # coordinates and do the thing
+        for r in range( rows):
+            for c in range (cols):
                 if (r,c) in pacific and (r,c) in atlantic:
                     res.append([r,c])
-
-
 
 
         return res
