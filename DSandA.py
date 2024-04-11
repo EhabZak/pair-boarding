6a720fef52d6b363ca07df71416b4f725fb4e081
ehab <ehab41976@gmail.com>
2024-04-11 12:08:19 -0700

    problems

--- DSandA.py
+++ DSandA.py
@@ -3268,6 +3268,102 @@ heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
 sol = Solution()
 print(sol.pacificAtlantic(heights))
 
+
+#! 39-1091. Shortest Path in Binary Matrix (Medium) (algo: matrix bfs)  (time complexity ???))
+'''
+1091. Shortest Path in Binary Matrix
+Medium
+Topics
+Companies
+Hint
+Given an n x n binary matrix grid, return the length of the shortest clear path in the matrix. If there is no clear path, return -1.
+
+A clear path in a binary matrix is a path from the top-left cell (i.e., (0, 0)) to the bottom-right cell (i.e., (n - 1, n - 1)) such that:
+
+All the visited cells of the path are 0.
+All the adjacent cells of the path are 8-directionally connected (i.e., they are different and they share an edge or a corner).
+The length of a clear path is the number of visited cells of this path.
+
+
+
+Example 1:
+
+
+Input: grid = [[0,1],[1,0]]
+Output: 2
+Example 2:
+
+
+Input: grid = [[0,0,0],[1,1,0],[1,1,0]]
+Output: 4
+Example 3:
+
+Input: grid = [[1,0,0],[1,1,0],[1,1,0]]
+Output: -1
+
+
+Constraints:
+
+n == grid.length
+n == grid[i].length
+1 <= n <= 100
+grid[i][j] is 0 or 1
+'''
+
+
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        rows , cols = len(grid), len(grid[0])
+        if grid[0][0] ==1 or grid[rows-1][cols-1] ==1:
+            return -1
+        # 1. Initialize the queue with starting value(s)
+        # 2.pop current node from queue
+        # 3.process current node
+        # 4.push neighbors into queue if they are valid
+        # 5.repeat steps 2-4 until queue is empty
+        def inbound(r,c):
+            rowInbound = 0 <= r < rows
+            colInbound = 0 <= c < cols
+            return rowInbound and colInbound
+
+        queue = deque()
+        queue.append((0,0,1)) #(r,c,layer)
+        directions = ((1,0),(0,1),(-1,0),(0,-1),(1,1), (1,-1),(-1,1), (-1,-1))
+        visited = set()
+        visited.add((0,0))
+        print ('queue start',queue)
+
+
+        while queue:
+            row,col, level = queue.popleft() # destructure
+            print('level', level )
+            if row == rows-1 and col == cols-1:
+                return level
+
+            for dir in directions:
+                newRow = row+ dir[0]
+                newCol = col + dir[1]
+
+                if inbound(newRow, newCol) and grid[newRow][newCol] != 1 and (newRow, newCol) not in visited:
+                    print ('newrow , newcol', newRow,newCol)
+                    queue.append((newRow,newCol,level+1))
+                    print (queue)
+                    visited.add((newRow,newCol))
+
+        return -1
+
+
+
+
+
+
+grid = [[0,0,0],[1,1,0],[1,1,0]] # 4
+sol = Solution()
+print(sol.shortestPathBinaryMatrix(grid))
+
+
+
+
 #! 11-752. Open the Lock -(Lecture) - (Medium)
 #! 12-554. Brick Wall -(Lecture) - (Medium)
 #! 13-51. N-Queens -(Lecture)- (Hard)
--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -5,64 +5,119 @@ import heapq
 import math, random
 
 
+# class Solution:
+#     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+#         rows , cols = len(heights), len(heights[0])
+
+#         # dfs helper function the base cases
+#         def dfs(r,c,visited,prevHeight):
+#             if not inbound(r,c) or (r,c) in visited or heights[r][c]< prevHeight:
+#                 return
+
+#             visited.add((r,c))
+#             # directions
+#             directions = ((0,1),(0,-1), (1,0),(-1,0))
+#             for dir in directions:
+#                 newRow = r + dir[0]
+#                 newCol = c + dir[1]
+#                 dfs(newRow,newCol,visited,heights[r][c])
+
+#         # inbound helper function
+
+#         def inbound(r,c):
+#             rowIn = 0 <= r < rows
+#             colIn = 0 <= c < cols
+#             return rowIn and colIn
+
+#         res = []
+#         pacific = set()
+#         atlantic = set()
+#         # visit the orientations
+
+#         for c in range(cols):
+#         # north
+#             dfs(0,c,pacific,heights[0][c])
+#         #south
+#             dfs(rows-1,c, atlantic,heights[rows-1][c])
+
+
+
+#         for r in range(rows):
+
+#         #east
+#             dfs(r,0, pacific,heights[r][0])
+#         #west
+#             dfs(r,cols-1, atlantic,heights[r][cols-1])
+
+#         # create a list and sets
+
+
+#         # coordinates and do the thing
+#         for r in range(rows):
+#             for c in range(cols):
+#                 if (r,c) in pacific and (r,c) in atlantic:
+#                     res.append([r,c])
+
+#         return res
+
+
+
+
+# heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
+# sol = Solution()
+# print(sol.pacificAtlantic(heights))
+
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        rows , cols = len(heights),  len(heights[0])
+        rows, cols = len(heights), len(heights[0])
 
-        # dfs and base cases
-        def dfs (r,c, visited, prevHight):
-            if not inbound(r,c) or (r,c) in visited or heights[r][c]< prevHight:
+        # helper function dfs and base cases
+        def dfs(r,c,visited,prevHeight):
+            # basecases
+            if not inbound(r,c) or heights[r][c]< prevHeight or (r,c)in visited:
                 return
             visited.add((r,c))
 
             # directions
-            directions = ((0,1),(0,-1),(1,0),(-1,0))
+            directions =  ((0,1), (0,-1),(1,0), (-1,0))
             for dir in directions:
                 newRow = r + dir[0]
                 newCol = c + dir[1]
-                dfs(newRow,newCol,visited,heights[r][c])
+                dfs(newRow, newCol, visited, heights[r][c])
 
-
-
-        # inbound
+        # inbound helper function
         def inbound(r,c):
-            rowIn = 0 <= r < rows
+            rowIn =  0 <= r < rows
             colIn = 0 <= c < cols
             return rowIn and colIn
 
-
-
-
-        #sets
+        # create the sets and list
         res = []
         pacific = set()
         atlantic = set()
 
-        # starting points
+        # check orientation
+
+        # north
         for c in range(cols):
-            # north
             dfs(0,c,pacific, heights[0][c])
-
-            #south
-            dfs(rows-1,c,atlantic, heights[rows-1][c])
-
-
+        # south
+            dfs(rows-1,c , atlantic, heights[rows-1][c])
         for r in range(rows):
-            # west
-            dfs(r,0,pacific,heights[r][0])
+            #west
+            dfs(r,0, pacific,heights[r][0])
             #east
-            dfs(r,cols-1,atlantic,heights[r][cols-1])
-            pass
-        # coordinates and do the thing
-        for r in range( rows):
-            for c in range (cols):
+            dfs(r, cols-1, atlantic, heights[r][cols-1])
+
+
+        # coordinates and do the things
+        for r in range(rows):
+            for c in range(cols):
                 if (r,c) in pacific and (r,c) in atlantic:
                     res.append([r,c])
 
-
+        # return the res
         return res
-
-
 
 
 heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
