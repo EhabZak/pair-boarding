f00d4a9dcfa0f62c7e65496607b86a384e9124e5
ehab <ehab41976@gmail.com>
2024-04-12 00:49:45 -0700

    problems

--- DSandA.py
+++ DSandA.py
@@ -3314,7 +3314,7 @@ grid[i][j] is 0 or 1
 # class Solution:
 #     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
 #         rows , cols = len(grid), len(grid[0])
-#         if grid[0][0] ==1 or grid[rows-1][cols-1] ==1: #!
+#         if grid[0][0] ==1 or grid[rows-1][cols-1] ==1:
 #             return -1
 #         # 1. Initialize the queue with starting value(s)
 #         # 2.pop current node from queue
@@ -3361,7 +3361,95 @@ grid[i][j] is 0 or 1
 # sol = Solution()
 # print(sol.shortestPathBinaryMatrix(grid))
 
+#! 40- 994. Rotting Oranges  (Medium) (algo: matrix bfs)  (time complexity ???))
 
+'''
+994. Rotting Oranges
+Medium
+Topics
+Companies Amazon Lyft Adobe
+You are given an m x n grid where each cell can have one of three values:
+
+0 representing an empty cell,
+1 representing a fresh orange, or
+2 representing a rotten orange.
+Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+
+Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
+
+
+
+Example 1:
+
+
+Input: grid = [[2,1,1],[1,1,0],[0,1,1]]
+Output: 4
+Example 2:
+
+Input: grid = [[2,1,1],[0,1,1],[1,0,1]]
+Output: -1
+Explanation: The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens 4-directionally.
+Example 3:
+
+Input: grid = [[0,2]]
+Output: 0
+Explanation: Since there are already no fresh oranges at minute 0, the answer is just 0.
+
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 10
+grid[i][j] is 0, 1, or 2.
+
+'''
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows , cols = len(grid) , len(grid[0])
+        
+        def inbound(r,c):
+            rowInbound = 0 <= r < rows
+            colInbound = 0 <= c < cols
+            return rowInbound and colInbound
+        queue = deque()
+        freshOranges = 0
+        time = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] ==1 :
+                    freshOranges +=1
+                elif grid[r][c] ==2:
+                    queue.append((r,c,0)) #! I don't understand
+        print (queue)
+        print('fresh oranges No =',freshOranges)
+
+        directions = ((1,0),(0,1),(-1,0),(0,-1))
+        while queue:
+            r,c, level = queue.popleft() # don't forget level becomes zero again
+            time = level
+            for dir in directions:
+                newRow = r + dir[0]
+                newCol = c + dir[1]
+
+                if inbound(newRow,newCol) and grid[newRow][newCol] == 1:
+                    queue.append((newRow,newCol,level+1))
+                    print('queue ========',queue)
+                    grid[newRow][newCol] = 2
+                    freshOranges -= 1
+        return time if freshOranges == 0 else -1
+
+
+
+
+
+grid = [[2,1,1],[1,1,0],[0,1,1]] # 4
+grid = [[2,1,1],[2,1,0],[0,1,1]] # 3 # it takes them in pairs cause there are more than one starting node here
+
+sol = Solution()
+print(sol.orangesRotting(grid))
 
 
 #! 11-752. Open the Lock -(Lecture) - (Medium)
--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -5,4 +5,16 @@ import heapq
 import math, random
 
 
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        
 
+
+
+
+
+grid = [[2,1,1],[1,1,0],[0,1,1]] # 4
+grid = [[2,1,1],[2,1,0],[0,1,1]] # 3 # it takes them in pairs cause there are more than one starting node here
+
+sol = Solution()
+print(sol.orangesRotting(grid))
