53a27dd91aed762c2a6d4158f4b61f5c1ac84652
ehab <ehab41976@gmail.com>
2024-04-14 01:18:19 -0700

    problems

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -5,3 +5,28 @@ import heapq
 import math, random
 
 
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+       target = len(graph)-1
+       results= []
+
+       def backtrack( currentNode, path):
+           if currentNode == target:
+               results.append(list(path))
+               return
+           for neighbor in graph[currentNode]:
+               path.append(neighbor)
+               backtrack(neighbor,path)
+               path.pop()
+       path = [0]
+       backtrack(0,path)
+
+
+       return results
+
+
+
+graph = [[1,2],[3],[3],[]]
+
+sol = Solution()
+print(sol.allPathsSourceTarget(graph))
