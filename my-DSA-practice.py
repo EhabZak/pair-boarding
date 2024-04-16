b5b37ec1383e7857dacc9095cdd31ae2200f1a33
ehab <ehab41976@gmail.com>
2024-04-16 16:38:25 -0700

    problems

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -4,58 +4,49 @@ from collections import defaultdict, Counter,deque
 import heapq
 import math, random
 
+
 class Solution:
-    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        # rows,cols = len(mat), len(mat[0])
-        # directions = ((1,0),(0,1),(-1,0),(0,-1))
+    def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
+        red = defaultdict(list)
+        blue = defaultdict(list)
+        for src, dst in redEdges:
+            red[src].append(dst)
+        for src, dst in blueEdges:
+            blue[src].append(dst)
+        print ( 'red *******', red)
 
-        # def inbound(r,c):
-        #     rowInbound = 0 <= r < rows
-        #     colInbound = 0 <= c < cols
-        #     return rowInbound and colInbound
-        # queue = deque()
-        # for r in range(rows):
-        #     for c in range(cols):
-        #         if mat[r][c] ==0 :
-        #             queue.append((r,c))
-        #         else:
-        #             mat[r][c] = -1
-        # while queue:
-        #     r,c = queue.popleft()
+        answer = [-1 for _ in range(n)]
+        queue = deque ([(0,0,None)])
+        visited = set ([(0 , None)])
+        print ('queue', queue)
 
-        #     for dir in directions:
-        #         newRow = r + dir[0]
-        #         newCol = c + dir[1]
+        while queue:
+            print ('queue', queue)
 
-        #         if not inbound(newRow, newCol) or mat[newRow][newCol] != -1: continue
-        #         mat [newRow][newCol] = mat[r][c] +1
-        #         queue.append((newRow,newCol))
-        # return mat
-        rows,cols = len(mat), len(mat[0])
-        directions = ((0,1),(1,0),(0,-1),(-1,0))
-
-        m, n = len(mat), len(mat[0])
-
-        for r in range(m):
-            for c in range(n):
-                if mat[r][c] > 0:
-                    top = mat[r - 1][c] if r > 0 else math.inf
-                    left = mat[r][c - 1] if c > 0 else math.inf
-                    print ('mat====>', mat)
-                    mat[r][c] = min(top, left) + 1
-                    print (min(top, left))
-
-        for r in range(m - 1, -1, -1):
-            for c in range(n - 1, -1, -1):
-                if mat[r][c] > 0:
-                    bottom = mat[r + 1][c] if r < m - 1 else math.inf
-                    right = mat[r][c + 1] if c < n - 1 else math.inf
-                    mat[r][c] = min(mat[r][c], bottom + 1, right + 1)
-        return mat
+            node, length ,edgeColor = queue.popleft()
+            print('visited ^^^^^^', visited)
+            print ('queueafter pop %%%%%%%%%%%%%%%%%%%%%%%%%%', queue)
+            if answer[node] == -1:
+                answer[node] = length
+            if edgeColor != 'Red':
+                for neighbor in red[node]:
+                    print('neighbor =========',neighbor)
+                    if (neighbor,'Red') not in visited:
+                        visited.add((neighbor,'Red'))
+                        queue.append((neighbor,length+1,'Red'))
+            if edgeColor != 'Blue':
+                for neighbor in blue[node]:
+                    if (neighbor,'Blue') not in visited:
+                        visited.add((neighbor,'Blue'))
+                        queue.append((neighbor,length+1,'Blue'))
+        return answer
 
 
 
-mat = [[0,0,0],[0,1,0],[1,1,1]]
-# mat = [[0,0,0],[0,1,0],[0,0,0]]
+n = 3
+redEdges = [] #[0,1,-1]
+redEdges = [[0,1],[1,2]] #[0,1,-1]
+# blueEdges = [[0,1],[1,2]] #[0,1,-1]
+blueEdges = []
 sol = Solution()
-print(sol.updateMatrix(mat))
+print(sol.shortestAlternatingPaths(n,redEdges,blueEdges))
