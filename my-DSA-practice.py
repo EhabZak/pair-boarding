f32e7ed66068c20d5c300c4968f3592b86064b6c
ehab <ehab41976@gmail.com>
2024-04-18 15:32:41 -0700

    problems

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -8,29 +8,39 @@ import math, random
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
        nodeColors = {}
-       Len = len(graph)
+       print ('nodeColors', nodeColors) #{}
 
-       def colorNodes(node, currentColor):
-           if node in nodeColors:
-               return nodeColors[node] == currentColor
-           nodeColors[node] = currentColor
+##########################################################################
+       def colorNodes(node, currentColor): # recursive function
+           print ('node ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^', node)
+           if node in nodeColors: # Check if the node is already colored
+               print ('node in nodeColors #####',node)
+               print ('nodeColors[node] == currentColor =>$$$$$$$$$$',nodeColors[node] == currentColor)
+               return nodeColors[node] == currentColor # Check if the color matches
+           nodeColors[node] = currentColor # if node not in nodeColors then Color the current node with the given color
+           print ('nodeColors[node]==== ', nodeColors[node] )
+           print('currentColor *****', currentColor  )
 
            for neighbor in graph[node]:
                if not colorNodes(neighbor,not currentColor):
                    return False
            return True
-       for node in range(Len):
-           if node not in nodeColors and not colorNodes(node,True):
+##########################################################################
+       #! execution start
+       for node in range(len(graph)):
+           #Checks if the node is not already colored and colors it with True (considering it as part of set A).
+           print('colorNodes(node,True)@@@@@@@@@@@@@@@@@@@@@@@',colorNodes(node,True))
+           if node not in nodeColors and not colorNodes(node,True): #! why does it have to be true and red is not working?
                return False
+       print ('nodeColors at the end', nodeColors)
        return True
-
-
-
-
-
 
 
 # graph = [[1,2,3],[0,2],[0,1,3],[0,2]] # False
 graph = [[1,3],[0,2],[1,3],[0,2]] #True
+# graph = [[1,3],[0,2],[1,3],[0,2],[4,5,6]] #True , this is working fine
+# graph = [[1,3],[0,2],[1,3],[0,2],[5,6]] #! not working why?
+
+#
 sol = Solution()
 print(sol.isBipartite(graph))
