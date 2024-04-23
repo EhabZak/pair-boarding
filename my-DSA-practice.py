79b31dd95e3e6c6f59da4c2c404c4c273af449ab
ehab <ehab41976@gmail.com>
2024-04-22 22:43:21 -0700

    problems

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -5,28 +5,4 @@ import heapq
 import math, random
 
 
-class Solution:
-    def fib(self, n: int, mono = {}) -> int:
 
-
-        if n ==1: return 1
-        elif n == 0: return 0
-        else:
-            x = [0,1]
-            y = 2
-
-            while y < n:
-                x.append(x[y-1]+x[y-2])
-                y +=1
-            return x[n-1]+x[n-2]
-
-
-
-
-
-
-
-n = 5
-solution = Solution()
-output = solution.fib(n)
-print(output)
