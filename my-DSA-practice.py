7d99aa3fbd4d028feccabf3c8d80b319a6e330bd
ehab <ehab41976@gmail.com>
2024-04-24 13:58:02 -0700

    problems

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -5,4 +5,22 @@ import heapq
 import math, random
 
 
+class Solution:
+    def climbStairs(self, n: int, memo ={}) -> int:
+        if n == 1: return 1
+        if n == 2: return 2
+        if n in memo: return memo[n]
 
+        memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+
+        return memo[n]
+
+
+
+
+
+n = 3
+
+solution = Solution()
+output = solution.climbStairs(n)
+print(output)
