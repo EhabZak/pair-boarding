3990aea1a4c15c9062005400fce34d8de902fc66
ehab <ehab41976@gmail.com>
2024-05-03 13:42:57 -0700

    problems

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -7,14 +7,37 @@ import math, random
 class WordDictionary:
 
     def __init__(self):
-        current = self.root
+        self.root = Node()
 
 
     def addWord(self, word: str) -> None:
+        current = self.root
+
+        for char in word:
+            if char not in current.children:
+                current.children[char] = Node()
+            current = current.children[char]
+        current.endOfWord = True
 
 ###########################################################
     def search(self, word: str) -> bool:
-        pass
+        def dfs(index,root):
+            current = root
+
+            for i in range(index,len(word)):
+                char = word[i]
+                if char == '.':
+                    for child in current.children.values():
+                        if dfs(i+1,child):
+                            return True
+                    return False
+                else:
+                    if char not in current.children:
+                        return False
+                    current = current.children[char]
+            return current.endOfWord
+
+        return dfs(0,self.root)
 
 
 
