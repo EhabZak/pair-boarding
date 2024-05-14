e150af39f002b32a32160a0c984e7b17f35ec87b
ehab <ehab41976@gmail.com>
2024-05-14 13:32:03 -0700

    playwright

--- IBM/PlayWrightAuto/tests/5_Locators_Builtin.spec.js
+++ IBM/PlayWrightAuto/tests/5_Locators_Builtin.spec.js
@@ -5,9 +5,11 @@ page.getByPlaceholder() to locate an input by placeholder.
 page.getByRole() to locate by explicit and implicit accessibility attributes.(actionable elements like button)
 page.getByText() to locate by text content.
 page.getByLabel() to locate a form control by associated label's text.
+       await page.getByLabel('Password').fill('secret');
 page.getByTitle() to locate an element by its title attribute.
+        await expect(page.getByTitle('Issues count')).toHaveText('25 issues');
 page.getByTestId() to locate an element based on its data-testid attribute (other attributes can be configured).
-
+       <button data-testid="directions">Itinéraire</button>
 */
 
 
@@ -18,14 +20,22 @@ test ('Builtin-locators', async({page})=>{
 
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
-    //page.getByAltText() to locate an element, usually image, by its text alternative.
+    //1-page.getByAltText() to locate an element, usually image, by its text alternative.
     const  logo = await page.getByAltText('company-branding')
 
     await expect(logo).toBeVisible()
 
-    // page.getByPlaceholder() to locate an input by placeholder.
+    // 2-page.getByPlaceholder() to locate an input by placeholder.
      await page.getByPlaceholder('Username').fill('Admin')
      await page.getByPlaceholder('password').fill('admin123')
+     await page.getByRole('button', {type:'submit'}).click()
+    //  await page.getByRole('button', {class:'oxd-button oxd-button--medium oxd-button--main orangehrm-login-button'}).click()
+
+    // 3-page.getByText() to locate by text content.
+    const name = await page.locator("//p[@class='oxd-userdropdown-name']").textContent()
+    await expect( await page.getByText(name)).toBeVisible()
+
+    await page.getByLabel('Password').fill('secret');
 
     await page.close()
 
