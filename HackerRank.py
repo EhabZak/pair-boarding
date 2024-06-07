1177852e64200e319c68e898741e5dfa1a2c175d
ehab <ehab41976@gmail.com>
2024-06-07 11:16:40 -0700

    rank

--- HackerRank.py
+++ HackerRank.py
@@ -317,6 +317,12 @@ Sample Input
 0
 Sample Output
 
+2147483648
+4294967294
+4294967295
+Explanation
 
+Take 1 for example, as unsigned 32-bits is 00000000000000000000000000000001
+and doing the flipping we get 11111111111111111111111111111110 which in turn is 4294967294.
 
 '''
