bb638c63d56df03dcd4ff74dfdd068e6f79fae0a
ehab <ehab41976@gmail.com>
2024-07-06 16:40:56 -0700

    problems

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -5,3 +5,28 @@ import heapq
 import math, random
 
 
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+       window = set()
+       left,right =  0
+       max_l = 0
+
+       while right < len(s):
+           while s[right] in window:
+               window.discard(s[left])
+               left +=1
+               max_l = max(max_l, right-left+1)
+               window.add(s[right])
+               right +=1
+               return max_l
+
+'''
+in this question we used a while loop in the 2nd loop and not if
+ because The while loop continues to execute as long as the condition s[right] in window is true
+ The if statement is executed only once per outer loop iteration.
+'''
+
+s = "abcabcbb"
+solution = Solution()
+output = solution.lengthOfLongestSubstring(s)
+print(output)
