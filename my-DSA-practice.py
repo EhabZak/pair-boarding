d8156588feb3ffc2dc8829b011bde5f11f0e91c5
ehab <ehab41976@gmail.com>
2024-07-06 15:46:53 -0700

    problems

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -8,16 +8,8 @@ import math, random
 class Solution(object):
     def containsNearbyDuplicate(self, nums, k):
         window = set()
-        left = 0
-
-        for right in range(len(nums)):
-            if right - left > k:
-                window.discard(nums[left])
-                left +=1
-            if nums[right] in window:
-                return True
-            window.add(nums[right])
-        return False
+        Left = 0
+        
 
 
 
