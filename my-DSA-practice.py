4ad17243d37d69dacf6c97a35c36700ffb6f22e4
ehab <ehab41976@gmail.com>
2024-07-22 13:49:53 -0700

    problems

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -118,26 +118,16 @@ Explanation: There is no way to partition the nodes into two independent sets su
 
 
 
-# def fib_tab(n):
-#     if n <= 2: return n
 
-#     dp = [0] * (n+1)
-#     print (dp)
-#     dp[1]= 1
-
-#     for index in range(2, n+1):
-#         dp[index] = dp[index-1]+dp[index-2]
-#     print (dp)
-#     return dp[n]
 
 def fib_tab(n):
-    if n < 2: return n
-    dp = [0]*(n+1)
+    if n <2 : return n
+    dp = [0] *(n+1)
     dp[1] =1
     for index in range (2, n+1):
         dp[index]= dp[index-1]+dp[index-2]
-    return dp[n]
 
+    return dp[n]
 
 
 
