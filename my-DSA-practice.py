606507c04f36c3b95a40bad17ca576046a54b533
ehab <ehab41976@gmail.com>
2024-07-22 13:47:26 -0700

    problems

--- my-DSA-practice.py
+++ my-DSA-practice.py
@@ -134,7 +134,9 @@ def fib_tab(n):
     if n < 2: return n
     dp = [0]*(n+1)
     dp[1] =1
-    
+    for index in range (2, n+1):
+        dp[index]= dp[index-1]+dp[index-2]
+    return dp[n]
 
 
 
