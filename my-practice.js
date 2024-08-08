667181b64e6369f244c5186e8fa0720c50dd5c62
ehab <ehab41976@gmail.com>
2024-08-08 12:17:53 -0700

    hackerrank

--- my-practice.js
+++ my-practice.js
@@ -23,4 +23,19 @@ function diagonalDifference(arr) {
 
 
         //     sum1 += arr[i][i]
+        //     sum2 += arr[i][arr.length -1 -i]
+        // }
 
+        // let result1 = Math.abs(sum1-sum2)
+        // return result1
+
+    }
+
+
+    let arr1 = [
+        // [3],
+        [11, 2, 4],
+        [4 ,5 ,6],
+        [10, 8, -12]
+    ]
+    console.log(diagonalDifference(arr1))
