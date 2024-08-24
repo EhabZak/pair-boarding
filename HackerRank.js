79615208af270061425537bb90e3763fe14441d3
ehab <ehab41976@gmail.com>
2024-08-24 09:26:15 -0400

    hacker rank

--- HackerRank.js
+++ HackerRank.js
@@ -353,5 +353,8 @@ algorithm cannot beat  (worst-case) running time, since  represents
 the minimum number of comparisons needed to know where to place each
 element. For more details, you can see these notes (PDF).
 
+Alternative Sorting
+Another sorting method, the counting sort, does not require comparison. Instead, you create an integer array whose index range covers the entire range of values in your array to sort. Each time a value occurs in the original array, you increment the counter at that index. At the end, run through your counting array, printing the value of each non-zero valued index that number of times.
+
 
 */
