b221d3e27940d7c011f059584961deb3793b414b
ehab <ehab41976@gmail.com>
2024-08-24 09:26:49 -0400

    hacker rank

--- HackerRank.js
+++ HackerRank.js
@@ -356,5 +356,17 @@ element. For more details, you can see these notes (PDF).
 Alternative Sorting
 Another sorting method, the counting sort, does not require comparison. Instead, you create an integer array whose index range covers the entire range of values in your array to sort. Each time a value occurs in the original array, you increment the counter at that index. At the end, run through your counting array, printing the value of each non-zero valued index that number of times.
 
+Example
+
+All of the values are in the range , so create an array of zeros, . The results of each iteration follow:
+
+i	arr[i]	result
+0	1	[0, 1, 0, 0]
+1	1	[0, 2, 0, 0]
+2	3	[0, 2, 0, 1]
+3	2	[0, 2, 1, 1]
+4	1	[0, 3, 1, 1]
+The frequency array is . These values can be used to create the sorted array as well: .
+
 
 */
