f1fac76074fd95e192e4eb1504db7fa974fee9fc
ehab <ehab41976@gmail.com>
2024-08-24 09:28:24 -0400

    hacker rank

--- HackerRank.js
+++ HackerRank.js
@@ -374,5 +374,17 @@ For this exercise, always return a frequency array with 100 elements. The exampl
 Challenge
 Given a list of integers, count and return the number of times each value appears as an array of integers.
 
+Function Description
+
+Complete the countingSort function in the editor below.
+
+countingSort has the following parameter(s):
+
+arr[n]: an array of integers
+Returns
+
+int[100]: a frequency array
+Input Format
+
 
 */
