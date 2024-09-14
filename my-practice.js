90c36377ec56eb0bb3b912500d23d9ff3f6c48bf
ehab <ehab41976@gmail.com>
2024-09-14 10:22:12 -0400

    hackerrank

--- my-practice.js
+++ my-practice.js
@@ -76,6 +76,18 @@ i	arr[i]	result
 4	1	[0, 3, 1, 1]
 The frequency array is . These values can be used to create the sorted array as well: .
 
+Note
+For this exercise, always return a frequency array with 100 elements. The example above shows only the first 4 elements, the remainder being zeros.
+
+Challenge
+Given a list of integers, count and return the number of times each value appears as an array of integers.
+
+Function Description
+
+Complete the countingSort function in the editor below.
+
+countingSort has the following parameter(s):
+
 
 
 
