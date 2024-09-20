c34e5db2e049ae551e1e2f1fd7d20601b6c4fbb9
ehab <ehab41976@gmail.com>
2024-09-20 15:10:57 -0400

    hackerrank

--- my-practice.js
+++ my-practice.js
@@ -68,3 +68,16 @@ Example
 
 All of the values are in the range , so create an array of zeros, . The results of each iteration follow:
 
+
+i	arr[i]	result
+0	1	[0, 1, 0, 0]
+1	1	[0, 2, 0, 0]
+2	3	[0, 2, 0, 1]
+3	2	[0, 2, 1, 1]
+4	1	[0, 3, 1, 1]
+The frequency array is . These values can be used to create the sorted array as well: .
+
+Note
+For this exercise, always return a frequency array with 100 elements. The example above shows only the first 4 elements, the remainder being zeros.
+
+
