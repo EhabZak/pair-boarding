6bf126da526b45e8fd02f712439ec651b7748975
ehab <ehab41976@gmail.com>
2024-09-20 15:11:23 -0400

    hackerrank

--- my-practice.js
+++ my-practice.js
@@ -81,3 +81,13 @@ Note
 For this exercise, always return a frequency array with 100 elements. The example above shows only the first 4 elements, the remainder being zeros.
 
 
+Challenge
+Given a list of integers, count and return the number of times each value appears as an array of integers.
+
+Function Description
+
+Complete the countingSort function in the editor below.
+
+countingSort has the following parameter(s):
+
+
