975d030051508daf821000cc18b093ee08c3dc83
ehab <ehab41976@gmail.com>
2024-10-26 11:49:51 -0400

    hackerrank

--- Scrape.py
+++ Scrape.py
@@ -14,28 +14,33 @@ def insertionSort(b):
             j -= 1 # j = j-1 => j = -1 exit the while loop since j < 0
             b[j+1] = up # b[0] = 0.1234
     return b
+#! x = [0.897, 0.565, 0.656,0.1234, 0.665, 0.3434]
 ######################################
 def bucketSort(x):
     arr = []
     slot_num = 10 # Number of buckets
+    # create the buckers
     for i in range(slot_num):
         arr.append([])
-        # print(arr)
+        print('arr is' ,arr)
 
     # Put array elements into different buckets
     for j in x:
-        print( '==jj=', j)
+        # print( '==jj=', j)
         index_b = int(slot_num*j) # Index of the bucket
+        # print ("index b ==>", index_b)
         arr[index_b].append(j)
-        print(arr)
+        # print(arr)
 
     # Sort individual buckets using insertion sort
     for i in range(slot_num):
+        # print ('arr[i]', arr[i])
         arr[i]= insertionSort(arr[i])
 
     # Concatenate sorted buckets into the original array
     k = 0
     for i in range (slot_num):
+        # print ('slotNumber', slot_num)
         for j in range(len(arr[i])):
             x[k] = arr[i][j]
             k +=1
@@ -44,21 +49,21 @@ def bucketSort(x):
 x = [0.897, 0.565, 0.656,
 	0.1234, 0.665, 0.3434]
 
-# print("Sorted Array is")
-# print(bucketSort(x))
+print("Sorted Array is")
+print(bucketSort(x))
 
 
-def insertionSort2(bb):
-    for i in range (1, len(bb)):
-        up = bb[i]
-        j = i-1
-        while j >= 0 and bb[j]> up:
-            bb[j+1] = bb[j]
-            j = j-1
-            bb[j+1]= up
-    return bb
+# def insertionSort2(bb):
+#     for i in range (1, len(bb)):
+#         up = bb[i]
+#         j = i-1
+#         while j >= 0 and bb[j]> up:
+#             bb[j+1] = bb[j]
+#             j = j-1
+#             bb[j+1]= up
+#     return bb
 
 
 
-b2= [0.3434, 0.1234, 0.565, 0.665, 0.656]
-print(insertionSort2(b2))
+# b2= [0.3434, 0.1234, 0.565, 0.665, 0.656]
+# print(insertionSort2(b2))
