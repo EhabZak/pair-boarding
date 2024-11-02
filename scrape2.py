a7d27def5cb77bdaf322ecfd028462b0e48c2f6c
ehab <ehab41976@gmail.com>
2024-11-02 09:36:19 -0400

    hackerrank

--- scrape2.py
+++ scrape2.py
@@ -14,4 +14,30 @@ def insertionSort(arr):
             j-=1
 return arr
 
+x = [0.897, 0.565, 0.656,0.1234, 0.665, 0.3434]
+print (insertionSort(x))
+
+# def bucketSort(x):
+#     array = []
+#     slot_num = 10
+#     for i in range (slot_num):
+#         array.append([])
+
+#     for j in x:
+#         index_b = int(slot_num*j)
+#         array[index_b].append(j)
+
+
+#     for i in range(slot_num):
+#         array[i]= insertionSort(array[i])
+
+#     k = 0
+#     for i in range (slot_num):
+#         for j in range (len(array[i])):
+#             x[k]= array[i][j]
+#             k +=1
+#     return x
+
+
+
 
