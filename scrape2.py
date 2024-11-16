8b7823e1e4cb2da6e0fffd7635d777fa0c899fc2
ehab <ehab41976@gmail.com>
2024-11-16 01:54:32 -0500

    hackerrank

--- scrape2.py
+++ scrape2.py
@@ -61,4 +61,12 @@ Another sorting method, the counting sort, does not require comparison. Instead,
 
 Example
 
+All of the values are in the range , so create an array of zeros, . The results of each iteration follow:
+
+i	arr[i]	result
+0	1	[0, 1, 0, 0]
+1	1	[0, 2, 0, 0]
+2	3	[0, 2, 0, 1]
+3	2	[0, 2, 1, 1]
+4	1	[0, 3, 1, 1]
 
