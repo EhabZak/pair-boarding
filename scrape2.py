55d5948ef7e12d1b6d895a501be610c1768437ef
ehab <ehab41976@gmail.com>
2024-12-21 08:18:38 -0500

    hackerrank

--- scrape2.py
+++ scrape2.py
@@ -6,11 +6,8 @@ def insertionSort(arr):
             # arr[j+1] = arr[j]
             # j = j -1
             # arr[j+1]= arr[i]
-      
-
-
-# x = [0.897, 0.565, 0.656,
-# 	0.1234, 0.665, 0.3434]
-
-# print("Sorted Array is")
-# print(bucketSort(x))git s
+        # ele = arr[i]
+        # while j >= 0 and arr[j]> ele:
+        #     arr[i] = arr[j]
+        #     arr[j] = ele
+          
