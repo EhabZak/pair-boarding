8ad06e4ee414afd54d68a3190719fc0b27867f73
ehab <ehab41976@gmail.com>
2025-01-04 17:27:14 -0500

    hackerrank

--- scrape2.py
+++ scrape2.py
@@ -2,3 +2,9 @@
 def insertionSort(arr):
     for i in range(1,len(arr)):
         j = i-1
+        print(arr[j])
+        while j >= 0 and arr[j]> arr[i]:
+            arr[j+1] = arr[j]
+            j = j -1
+            arr[j+1]= arr[i]
+       
